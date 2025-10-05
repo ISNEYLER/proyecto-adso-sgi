e33649dbeb6d4ab630b340158e42a1fb8ec52019
Isneyler Mosquera <isneyler@gmail.com>
2025-10-04 20:53:17 -0500

    Se crearon las diferentes rutas de la app y se vincularon a los controladores

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,12 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
 $routes->get('/', 'Home::index');
+$routes->get('dashboard', 'Dashboard::index');
+$routes->get('products', 'Products::index');
+$routes->get('transfers', 'Transfers::index');
+$routes->get('disposal', 'Disposal::index');
+$routes->get('inventory_adj', 'InventoryAdjustment::index');
+$routes->get('settings', 'Settings::index');
+$routes->get('user', 'User::index');
