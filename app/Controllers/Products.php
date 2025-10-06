c44aca1d287adead894989df7765bcbd5b4ac968
Isneyler Mosquera <isneyler@gmail.com>
2025-10-06 09:15:14 -0500

    Actualizado controlador Products y vista Products

--- app/Controllers/Products.php
+++ app/Controllers/Products.php
@@ -5,7 +5,8 @@ namespace App\Controllers;
 class Products extends BaseController
 {
     public function index(): string
-    {
-        return view('welcome_message');
+    {   
+        $data = ["title" => 'Productos'];
+        return view('products/index', $data);
     }
 }
---
+++ app/Views/products/index.php
@@ -0,0 +1,5 @@
+<?php echo $this->extend('templates/layout'); ?>
+
+<?php echo $this->section('content'); ?>
+
+<?php echo $this->endSection(); ?>
