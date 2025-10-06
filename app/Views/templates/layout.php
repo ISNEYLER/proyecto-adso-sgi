faf6acaf9ddbff07ee06bd95f11eda499f2715de
Isneyler Mosquera <isneyler@gmail.com>
2025-10-06 15:45:32 -0500

    Añadido popper.js de forma local al proyecto

--- app/Views/templates/layout.php
+++ app/Views/templates/layout.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo($title); ?></title>
-    <link rel="stylesheet" href="../css/styles.css">
-    <link rel="stylesheet" href="../lib/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url('css/styles.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('lib/bootstrap/css/bootstrap.min.css') ?>">
 </head>
 <body>
     <header>
         <nav class="navbar navbar-expand-sm navbar-light bg-primary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">LOGO</a>
+                <a class="navbar-brand" href="<?= base_url('/')?>">LOGO</a>
                 <ul class="navbar-nav me-auto">
                     <li class="nav-item">
-                        <a href="#" class="nav-link">Productos</a>
+                        <a href="<?= base_url('products')?>" class="nav-link">Productos</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Operaciones</a>
@@ -41,7 +41,7 @@
     <?php echo($this->renderSection("content")); ?>
 
 </body>
-<script src="https://unpkg.com/@popperjs/core@2/dist/umd/popper.js"></script>
-<script src="../lib/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?php base_url('lib/popper/popper.min.js') ?>"></script>
+<script src="<?php base_url('lib/bootstrap/js/bootstrap.min.js') ?>"></script>
 <?php echo($this->renderSection("scripts")); ?>
 </html>
