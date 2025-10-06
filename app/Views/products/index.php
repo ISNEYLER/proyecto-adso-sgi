8e62e2e297e48678823c0dc42af1bbbdeb451734
Isneyler Mosquera <isneyler@gmail.com>
2025-10-06 09:46:19 -0500

    Se modificó la vista products

--- app/Views/products/index.php
+++ app/Views/products/index.php
@@ -1,5 +1,36 @@
 <?php echo $this->extend('templates/layout'); ?>
 
 <?php echo $this->section('content'); ?>
-
+  <div>
+    <div class="d-flex justify-content-between p-4 bg-body">
+      <div class="d-flex flex-column">
+        <H4>Productos</H4>
+      </div>
+      <div>
+        <a class="btn btn-success btn-sm" href="producto/crear-producto.html">Añadir Producto</a>
+      <input type="email" class="form-control-sm" id="exampleFormControlInput1" placeholder="Buscar">
+    </div>
+  </div>
+  <div class="table-responsive">
+    <table class="table table-striped">
+      <thead>
+        <tr class="table-dark">
+          <th>Producto</th>
+          <th>SKU</th>
+          <th>Cod Barras</th>
+          <th>Precio</th>
+          <th>Costo</th>
+          <th>Cantidad</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Barra de sonido Hisense HS1000</td>
+          <td>73261439</td>
+          <td>73261439</td>
+          <td>$ 329.000</td>
+          <td>$ 200.000</td>
+          <td>20</td>
+        </tr>
+  </div>
 <?php echo $this->endSection(); ?>
