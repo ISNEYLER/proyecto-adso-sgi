2b9f97bef9b399a3d1773655fb6e81e2293f9928
Isneyler Mosquera <isneyler@gmail.com>
2025-10-06 20:46:49 -0500

    Añadidas rutas nuevas y actualizadas las rutas antiguas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,7 +10,7 @@ $routes->get('/', 'Home::index');
 $routes->get('dashboard', 'Dashboard::index');
 $routes->get('products', 'Products::index');
 $routes->get('products/create', 'Products::create');
-$routes->get('transfers', 'Transfers::index');
+$routes->get('movements', 'Movements::index');
 $routes->get('disposal', 'Disposal::index');
 $routes->get('inventory_adj', 'InventoryAdjustment::index');
 $routes->get('settings', 'Settings::index');
