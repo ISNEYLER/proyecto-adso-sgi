668f5444bf6889ffa060da69452d07cf60343019
Isneyler Mosquera <isneyler@gmail.com>
2025-10-12 22:36:35 -0500

    Añadidas rutas y modificados los controladores

--- app/Controllers/Disposal.php
+++ app/Controllers/Disposal.php
@@ -7,6 +7,7 @@ class Disposal extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        $data = ['title'=> 'Desecho de Inventario'];
+        return view('disposal/index', $data);
     }
 }
--- app/Controllers/InventoryAdjustment.php
+++ app/Controllers/InventoryAdjustment.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\Product;
+use App\Models\Location;
+
 //Controlador de ajuste de inventario
 class InventoryAdjustment extends BaseController
 {
@@ -9,4 +12,15 @@ class InventoryAdjustment extends BaseController
         $data =['title'=> 'Ajuste de Inventario'];
         return view('inventory_adjustment/index',$data);
     }
+
+    public function create(): string{
+        $productoModel = new Product();
+        $products = $productoModel->findAll();
+        $locationModel = new Location();
+        $locations = $locationModel->findAll();
+
+        $data = ["title" => 'Crear ajuste de inventario','locations'=> $locations,'products'=> $products];
+        return view('inventory_adjustment/create',$data);
+    }
 }
+
