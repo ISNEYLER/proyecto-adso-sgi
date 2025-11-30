957cf48584348ecc2f738d7f8fb5e0c56634ab7e
Isneyler Mosquera <isneyler@gmail.com>
2025-11-30 10:10:32 -0500

    Modificado vista y controlador Dashboard

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -2,12 +2,47 @@
 
 namespace App\Controllers;
 
-//Controlador de Dashboard
+use App\Models\Product;
+use App\Models\Movement;
+use App\Models\Stock;
+
+
 class Dashboard extends BaseController
 {
     public function index(): string
     {
-        $data = ["title"=>"Dashboard"];
-        return view('dashboard',$data);
+        $productModel = new Product();
+        $movementModel = new Movement();
+        $stockModel = new Stock();
+        
+
+        // Contar productos
+        $totalProductos = $productModel->countAll();
+
+        // Contar movimientos
+        $totalMovimientos = $movementModel->countAll();
+
+        // Usamos tu método
+        $existencias = $stockModel->obtenerStock();
+
+        // FILTRAR PRODUCTOS CON STOCK BAJO ( <= 5 )
+        $stockBajo = array_filter($existencias, function($item) {
+            return $item->cantidad <= 5;
+        });
+
+        $ultimosMovimientos = $movementModel->obtenerMovimientosConNombresDash();
+
+        // $productosBajoStock = $productModel->productosBajoStock();
+
+        $data = [
+            "title"            => "Dashboard",
+            "totalProductos"   => $totalProductos,
+            "totalMovimientos" => $totalMovimientos,
+            'totalStockBajo'  => count($stockBajo),
+            'stockBajo'       => $stockBajo,
+            'ultimosMovimientos' => $ultimosMovimientos
+        ];
+
+        return view('dashboard', $data);
     }
 }
